bb2dc03bdb54e17cfe3d6423b3b9245543c1f6e8
Hassan Jahandideh <jahandideh.2083@gmail.com>
2025-01-11 22:53:13 +0300

    make form onSubmit

--- task-manager/src/components/TodoForm.jsx
+++ task-manager/src/components/TodoForm.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const TodoForm = () => {
+   const [value, setValue] = useState('');
 
+   const handleSubmit = e => {
+      e.preventDefault();
+      console.log(value);
+      
+   }
 
    return (
-      <form className="TodoForm">
-         <input type="text" className='todo-input' placeholder='what is the task today?' />
-         <button type="submit" className='todo-btn'>Add task</button>
+      <form className="TodoForm" onSubmit={handleSubmit}>
+         <input type="text" className='todo-input' onChange={(e) => setValue(e.target.value)} placeholder='what is the task today?' />
+         <button type="submit" className='todo-btn'>
+            Add task
+         </button>
       </form>
    );
 }
